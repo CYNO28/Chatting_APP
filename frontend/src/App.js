e746d7bd4cc157014c9088fe0adc6d0af58b150d
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-09-18 20:29:38 +0530

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './Component/Navbar/Navbar';
 import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div >
+      <Navbar/>  
 <Login></Login>
     </div>
   );
---
+++ frontend/src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+
+import React, { useRef } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import style from "./navbar.module.css";
+const Navbar = ({ scroller }) => {
+
+  return (
+    <>
+   
+    </>
+  );
+};
+
+export default Navbar;
+
